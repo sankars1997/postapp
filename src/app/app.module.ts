32df8674f20506cec95bc52ec847193159ee0c07
sankar <sankarsuresh1997@gmail.com>
2022-11-25 16:02:16 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { ViewComponent } from './view/view.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule,Routes } from '@angular/router';
+
+const myRoute:Routes =[
+  {
+path:"",
+component:AddComponent
+  },
+  {
+    path:"view",
+    component:ViewComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddComponent,
-    ViewComponent
+    ViewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
