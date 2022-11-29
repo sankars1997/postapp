bb6ce1871d17b09be8c91cc20fa0c208b3a2503b
sankar <sankarsuresh1997@gmail.com>
2022-11-29 15:18:29 +0530

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
@@ -28,7 +28,7 @@ component:AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,RouterModule.forRoot(myRoute)
+    AppRoutingModule,RouterModule.forRoot(myRoute),HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
